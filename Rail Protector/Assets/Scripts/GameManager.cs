c6c2111e95336791bbc691e12b87b8a1686cee57
RemiJu <robergaby@hotmail.es>
2023-09-18 19:05:34 -0400

    Updated Game Manager Script

--- Rail Protector/Assets/Scripts/GameManager.cs
+++ Rail Protector/Assets/Scripts/GameManager.cs
@@ -5,11 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
     public Animator transAnim;
     // Start is called before the first frame update
     void Awake()
     {
-       
+       if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+       else
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void NextLevel()
